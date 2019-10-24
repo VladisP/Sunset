fb9a62e5989499004afaf893bb895c8fcd9eb4c7
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-24 16:29:53 +0300

    [ADD] Простая анимация

--- app/src/main/java/com/example/sunset/SunsetFragment.java
+++ app/src/main/java/com/example/sunset/SunsetFragment.java
@@ -1,5 +1,6 @@
 package com.example.sunset;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,10 @@ import androidx.fragment.app.Fragment;
 
 public class SunsetFragment extends Fragment {
 
+    private View mSceneView;
+    private View mSunView;
+    private View mSkyView;
+
     public static SunsetFragment newInstance() {
         return new SunsetFragment();
     }
@@ -19,5 +24,30 @@ public class SunsetFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_sunset, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mSceneView = view;
+        mSunView = view.findViewById(R.id.sun);
+        mSkyView = view.findViewById(R.id.sky);
+
+        mSceneView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startAnimation();
+            }
+        });
+    }
+
+    private void startAnimation() {
+        float sunYStart = mSunView.getTop();
+        float sunYEnd = mSkyView.getHeight();
+
+        ObjectAnimator heightAnimator = ObjectAnimator
+                .ofFloat(mSunView, "y", sunYStart, sunYEnd)
+                .setDuration(3000);
+
+        heightAnimator.start();
     }
 }
