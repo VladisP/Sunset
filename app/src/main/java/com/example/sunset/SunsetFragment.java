3f7fabbf8c0c1f6e492e1f4965b2308a311028ef
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-24 16:07:20 +0300

    [ADD] Окончательное построение сцены

---
+++ app/src/main/java/com/example/sunset/SunsetFragment.java
@@ -0,0 +1,23 @@
+package com.example.sunset;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SunsetFragment extends Fragment {
+
+    public static SunsetFragment newInstance() {
+        return new SunsetFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_sunset, container, false);
+    }
+}
