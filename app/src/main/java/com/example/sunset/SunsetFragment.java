7f234eff39c814ab743a6ff89f904c5ce58a4a40
Pichugin Vladislav <vlad19041999@yandex.ru>
2019-10-24 16:41:04 +0300

    [ADD] Смена цвета неба при заходе солнца

--- app/src/main/java/com/example/sunset/SunsetFragment.java
+++ app/src/main/java/com/example/sunset/SunsetFragment.java
@@ -1,6 +1,8 @@
 package com.example.sunset;
 
+import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +18,10 @@ public class SunsetFragment extends Fragment {
     private View mSceneView;
     private View mSunView;
     private View mSkyView;
+
+    private int mBlueSkyColor;
+    private int mSunsetSkyColor;
+    private int mNightSkyColor;
 
     public static SunsetFragment newInstance() {
         return new SunsetFragment();
@@ -33,6 +39,11 @@ public class SunsetFragment extends Fragment {
         mSunView = view.findViewById(R.id.sun);
         mSkyView = view.findViewById(R.id.sky);
 
+        Resources resources = getResources();
+        mBlueSkyColor = resources.getColor(R.color.blue_sky);
+        mSunsetSkyColor = resources.getColor(R.color.sunset_sky);
+        mNightSkyColor = resources.getColor(R.color.night_sky);
+
         mSceneView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,8 +59,14 @@ public class SunsetFragment extends Fragment {
         ObjectAnimator heightAnimator = ObjectAnimator
                 .ofFloat(mSunView, "y", sunYStart, sunYEnd)
                 .setDuration(3000);
-
         heightAnimator.setInterpolator(new AccelerateInterpolator());
+
+        ObjectAnimator sunsetSkyAnimator = ObjectAnimator
+                .ofInt(mSkyView, "backgroundColor", mBlueSkyColor, mSunsetSkyColor)
+                .setDuration(3000);
+        sunsetSkyAnimator.setEvaluator(new ArgbEvaluator());
+
         heightAnimator.start();
+        sunsetSkyAnimator.start();
     }
 }
